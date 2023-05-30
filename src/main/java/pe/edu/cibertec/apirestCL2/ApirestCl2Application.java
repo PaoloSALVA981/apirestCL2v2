b45905822008485855a71566ef491630d4c84cc2
PaoloSALVA981 <sanchezpaolo878@gmail.com>
2023-05-30 04:06:26 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/apirestCL2/ApirestCl2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.apirestCL2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestCl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestCl2Application.class, args);
+	}
+
+}
